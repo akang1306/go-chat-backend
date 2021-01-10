5fc4491c6151c8affb74a3a817b9b3f49950b210
Antonio Kang <akang1306@gmail.com>
2021-01-09 22:03:48 -0300

    initial validateUser middleware implementation

--- pkg/auth/validator.go
+++ pkg/auth/validator.go
@@ -1,12 +1,30 @@
 package auth
 
-import "net/http"
+import (
+	"context"
+	"net/http"
+	"strings"
+
+	"github.com/challenge/pkg/jwt"
+)
+
+type tokenValidator interface {
+	VerifyToken(tokenString string) (int, error)
+}
+
+const authorizationHeader string = "Authorization"
 
 // ValidateUser checks for a token and validates it
 // before allowing the method to execute
-func ValidateUser(_ http.HandlerFunc) http.HandlerFunc {
+func ValidateUser(handler http.HandlerFunc) http.HandlerFunc {
+	validator := jwt.New()
 	return func(w http.ResponseWriter, r *http.Request) {
-		// TODO: validate token
-		http.Error(w, "Invalid token", http.StatusUnauthorized)
+		token := strings.TrimSpace(r.Header.Get("Authorization"))
+		id, err := validator.VerifyToken(token)
+		if err != nil {
+			http.Error(w, "Invalid token", http.StatusUnauthorized)
+			return
+		}
+		handler.ServeHTTP(w, r.WithContext(context.WithValue(r.Context(), "user", id)))
 	}
 }
