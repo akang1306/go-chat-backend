85180ec4f736f8a6f5c4686594c6187d3b0d5077
Antonio Kang <akang1306@gmail.com>
2021-01-11 15:19:15 -0300

    jwt updates

--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -1,6 +1,9 @@
 package jwt
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
 	"time"
 
 	"github.com/challenge/pkg/models"
@@ -21,13 +24,12 @@ type Token struct {
 // TokenForUser returns a token valid for the given user with default expiration.
 func (Manager) TokenForUser(user *models.User) string {
 	expiresAt := time.Now().Add(defaultExpiration).Unix()
-	tokenClaims := &Token{
-		UserID: user.ID,
-		StandardClaims: &jwt.StandardClaims{
-			ExpiresAt: expiresAt,
-		},
-	}
-	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), tokenClaims)
+
+	claims := jwt.MapClaims{}
+	claims["user_id"] = user.ID
+	claims["exp"] = expiresAt
+
+	token := jwt.NewWithClaims(jwt.GetSigningMethod("HS256"), claims)
 	tokenString, err := token.SignedString([]byte(secretKey))
 	if err != nil {
 		return err.Error()
@@ -37,14 +39,21 @@ func (Manager) TokenForUser(user *models.User) string {
 
 // VerifyToken checks the tokenString and returns userID if valid, error if not.
 func (Manager) VerifyToken(tokenString string) (int, error) {
-	token := Token{}
-	_, err := jwt.ParseWithClaims(tokenString, token, func(token *jwt.Token) (interface{}, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		return []byte(secretKey), nil
 	})
 	if err != nil {
 		return 0, err
 	}
-	return token.UserID, nil
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok || !token.Valid {
+		return 0, errors.New("invalid token")
+	}
+	userID, err := strconv.ParseInt(fmt.Sprintf("%.f", claims["user_id"]), 10, 64)
+	if err != nil {
+		return 0, err
+	}
+	return int(userID), nil
 }
 
 func New() Manager {
