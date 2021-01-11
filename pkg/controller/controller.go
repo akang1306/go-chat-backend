582278d4a67f83b2489fbf19568d470480e8c803
Antonio Kang <akang1306@gmail.com>
2021-01-11 15:33:03 -0300

    service interface method

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -10,6 +10,7 @@ type serviceHandler interface {
 	CreateUser(ctx context.Context, username, password string) (*models.User, error)
 	Login(ctx context.Context, username, password string) (*models.Login, error)
 	Health(ctx context.Context) models.Health
+	SendMessage(ctx context.Context, sender, recipient int, content models.MessageContent) (*models.Message, error)
 }
 
 // Handler provides the interface to handle different requests
